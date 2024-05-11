ec8deeaf20f28a1815af7b483503283ff9f31439
SashaTyutyaev <sashatut2004@yandex.ru>
2024-05-11 03:30:50 +0300

    optimize imports

--- src/test/java/ru/yandex/practicum/filmorate/dao/user/UserDaoImplTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/dao/user/UserDaoImplTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
